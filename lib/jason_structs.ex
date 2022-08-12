a26f97a433d9a537826a19ce5db00c132d56b51d
Evan O'Brien <evanbobrien@gmail.com>
2022-08-12 21:20:18 +0100

    Disable ability to decode into Jason.Struct

--- lib/jason_structs.ex
+++ lib/jason_structs.ex
@@ -107,83 +107,83 @@ defmodule Jason.Structs do
   @spec encode!(term(), [Jason.encode_opt()]) :: String.t() | no_return()
   defdelegate encode!(term, encode_opts \\ []), to: Jason
 
-  @doc """
-  Parses a JSON value from input iodata and if a `struct_module` is provided,
-  which is a `Jason.Structs.Struct` implementation, the parsed result will be an
-  instance of that struct.
+  # @doc """
+  # Parses a JSON value from input iodata and if a `struct_module` is provided,
+  # which is a `Jason.Structs.Struct` implementation, the parsed result will be an
+  # instance of that struct.
 
-  By skipping the `struct_module` it works like the normal `Jason.decode` function.
-  It can parse all valid JSON values that way (numbers, lists, booleans, objects, strings, null).
+  # By skipping the `struct_module` it works like the normal `Jason.decode` function.
+  # It can parse all valid JSON values that way (numbers, lists, booleans, objects, strings, null).
 
-  Examples
+  # Examples
 
-      defmodule Country do
-        use Jason.Structs.Struct
+  #     defmodule Country do
+  #       use Jason.Structs.Struct
 
-        jason_struct do
-          field(:code, String.t(), enforce: true)
-          field(:name, String.t(), enforce: true)
-        end
-      end
+  #       jason_struct do
+  #         field(:code, String.t(), enforce: true)
+  #         field(:name, String.t(), enforce: true)
+  #       end
+  #     end
 
-      iex> json = ~s({"name": "Bulgaria", "code": "bg"})
-      iex> Jason.Structs.decode(json, Country)
-      {:ok, %Country{code: "bg", name: "Bulgaria"}}
+  #     iex> json = ~s({"name": "Bulgaria", "code": "bg"})
+  #     iex> Jason.Structs.decode(json, Country)
+  #     {:ok, %Country{code: "bg", name: "Bulgaria"}}
 
-      iex> json = ~s({"name": "Bulgaria", "code": "bg"})
-      iex> Jason.Structs.decode(json)
-      {:ok, %{code: "bg", name: "Bulgaria"}}
+  #     iex> json = ~s({"name": "Bulgaria", "code": "bg"})
+  #     iex> Jason.Structs.decode(json)
+  #     {:ok, %{code: "bg", name: "Bulgaria"}}
 
-      iex> json = "55"
-      iex> Jason.Structs.decode(json)
-      {:ok, 55}
+  #     iex> json = "55"
+  #     iex> Jason.Structs.decode(json)
+  #     {:ok, 55}
 
-      iex> Jason.Structs.decode("invalid")
-      {:error, %Jason.DecodeError{data: "invalid", position: 0, token: nil}}
-  """
-  @spec decode(iodata(), atom()) :: {:ok, term()} | {:error, DecodeError.t()}
-  defdelegate decode(iodata, struct_module \\ nil), to: Jason.Structs.Decoder
+  #     iex> Jason.Structs.decode("invalid")
+  #     {:error, %Jason.DecodeError{data: "invalid", position: 0, token: nil}}
+  # """
+  # @spec decode(iodata(), atom()) :: {:ok, term()} | {:error, DecodeError.t()}
+  # defdelegate decode(iodata, struct_module \\ nil), to: Jason.Structs.Decoder
 
-  @doc """
-  Parses a JSON value from input iodata and if a `struct_module` is provided,
-  which is a `Jason.Structs.Struct` implementation, the parsed result will be an
-  instance of that struct.
+  # @doc """
+  # Parses a JSON value from input iodata and if a `struct_module` is provided,
+  # which is a `Jason.Structs.Struct` implementation, the parsed result will be an
+  # instance of that struct.
 
-  Similar to `decode/2` except it will unwrap the error tuple and raise in case of errors.
+  # Similar to `decode/2` except it will unwrap the error tuple and raise in case of errors.
 
-  By skipping the `struct_module` it works like the normal `Jason.decode!` function.
-  It can parse all valid JSON values that way (numbers, lists, booleans, objects, strings, null).
+  # By skipping the `struct_module` it works like the normal `Jason.decode!` function.
+  # It can parse all valid JSON values that way (numbers, lists, booleans, objects, strings, null).
 
-  Examples
+  # Examples
 
-      defmodule Country do
-        use Jason.Structs.Struct
+  #     defmodule Country do
+  #       use Jason.Structs.Struct
 
-        jason_struct do
-          field(:code, String.t(), enforce: true)
-          field(:name, String.t(), enforce: true)
-        end
-      end
+  #       jason_struct do
+  #         field(:code, String.t(), enforce: true)
+  #         field(:name, String.t(), enforce: true)
+  #       end
+  #     end
 
-      iex> json = ~s({"name": "Bulgaria", "code": "bg"})
-      iex> Jason.Structs.decode!(json, Country)
-      %Country{code: "bg", name: "Bulgaria"}
+  #     iex> json = ~s({"name": "Bulgaria", "code": "bg"})
+  #     iex> Jason.Structs.decode!(json, Country)
+  #     %Country{code: "bg", name: "Bulgaria"}
 
-      iex> json = ~s({"name": "Bulgaria", "code": "bg"})
-      iex> Jason.Structs.decode!(json)
-      %{code: "bg", name: "Bulgaria"}
+  #     iex> json = ~s({"name": "Bulgaria", "code": "bg"})
+  #     iex> Jason.Structs.decode!(json)
+  #     %{code: "bg", name: "Bulgaria"}
 
-      iex> Jason.Structs.decode!("true")
-      true
+  #     iex> Jason.Structs.decode!("true")
+  #     true
 
-      iex> Jason.Structs.decode!("invalid")
-      ** (Jason.DecodeError) unexpected byte at position 0: 0x69 ('i')
-  """
-  @spec decode!(iodata(), atom()) :: term() | no_return()
-  def decode!(iodata, struct_module \\ nil) do
-    case decode(iodata, struct_module) do
-      {:ok, result} -> result
-      {:error, error} -> raise error
-    end
-  end
+  #     iex> Jason.Structs.decode!("invalid")
+  #     ** (Jason.DecodeError) unexpected byte at position 0: 0x69 ('i')
+  # """
+  # @spec decode!(iodata(), atom()) :: term() | no_return()
+  # def decode!(iodata, struct_module \\ nil) do
+  #   case decode(iodata, struct_module) do
+  #     {:ok, result} -> result
+  #     {:error, error} -> raise error
+  #   end
+  # end
 end
--- lib/jason_structs/typed_struct_plugin.ex
+++ lib/jason_structs/typed_struct_plugin.ex
@@ -22,65 +22,65 @@ defmodule Jason.Structs.TypedStructPlugin do
 
   @impl true
   @spec field(atom(), any(), keyword(), Macro.Env.t()) :: Macro.t()
-  def field(name, type, opts, _env) do
-    type =
-      if is_list(type) and Enum.count(type) == 1 do
-        List.first(type)
-      else
-        type
-      end
+  def field(name, _type, opts, _env) do
+    # type =
+    #   if is_list(type) and Enum.count(type) == 1 do
+    #     List.first(type)
+    #   else
+    #     type
+    #   end
 
-    module =
-      case type do
-        {{:., _, [{:__aliases__, _, [atom]}, :t]}, _, _}
-        when is_atom(atom) ->
-          module = Module.concat([:"Elixir", atom])
+    # module =
+    #   case type do
+    #     {{:., _, [{:__aliases__, _, [atom]}, :t]}, _, _}
+    #     when is_atom(atom) ->
+    #       module = Module.concat([:"Elixir", atom])
 
-          if module == opts[:this] do
-            module
-          else
-            functions = module.__info__(:functions)
+    #       if module == opts[:this] do
+    #         module
+    #       else
+    #         functions = module.__info__(:functions)
 
-            if Enum.member?(functions, {:sub_structs, 0}) do
-              module
-            else
-              nil
-            end
-          end
+    #         if Enum.member?(functions, {:sub_structs, 0}) do
+    #           module
+    #         else
+    #           nil
+    #         end
+    #       end
 
-        _ ->
-          nil
-      end
+    #     _ ->
+    #       nil
+    #   end
 
     is_excludable = opts[:excludable]
 
-    type_data =
-      case type do
-        {{:., _, [{:__aliases__, _, [atom]}, :t]}, _, _} ->
-          {:struct, atom}
+    # type_data =
+    #   case type do
+    #     {{:., _, [{:__aliases__, _, [atom]}, :t]}, _, _} ->
+    #       {:struct, atom}
 
-        {:|, _, values} ->
-          if Enum.all?(values, &is_atom/1) do
-            {:enum, values}
-          else
-            # TODO a sum type
-            {:sum, values}
-          end
+    #     {:|, _, values} ->
+    #       if Enum.all?(values, &is_atom/1) do
+    #         {:enum, values}
+    #       else
+    #         # TODO a sum type
+    #         {:sum, values}
+    #       end
 
-        {simple_type, _, _} ->
-          {:simple_type, simple_type}
-      end
+    #     {simple_type, _, _} ->
+    #       {:simple_type, simple_type}
+    #   end
 
     quote do
-      unless is_nil(unquote(module)) do
-        Module.put_attribute(__MODULE__, :sub_structs, {unquote(name), unquote(module)})
-      end
+      # unless is_nil(unquote(module)) do
+      #   Module.put_attribute(__MODULE__, :sub_structs, {unquote(name), unquote(module)})
+      # end
 
       if unquote(is_excludable) == true do
         Module.put_attribute(__MODULE__, :excludable_keys, unquote(name))
       end
 
-      Module.put_attribute(__MODULE__, :types_info, {unquote(name), unquote(type_data)})
+      # Module.put_attribute(__MODULE__, :types_info, {unquote(name), unquote(type_data)})
     end
   end
 
@@ -88,21 +88,21 @@ defmodule Jason.Structs.TypedStructPlugin do
   @spec after_definition(opts :: keyword()) :: Macro.t()
   def after_definition(_opts) do
     quote do
-      def sub_structs do
-        Map.new(@sub_structs)
-      end
+      # def sub_structs do
+      #   Map.new(@sub_structs)
+      # end
 
       def excludable_keys do
         @excludable_keys
       end
 
-      def type_data do
-        Map.new(@types_info)
-      end
+      # def type_data do
+      #   Map.new(@types_info)
+      # end
 
-      Module.delete_attribute(__MODULE__, :types_info)
+      # Module.delete_attribute(__MODULE__, :types_info)
       Module.delete_attribute(__MODULE__, :excludable_keys)
-      Module.delete_attribute(__MODULE__, :sub_structs)
+      # Module.delete_attribute(__MODULE__, :sub_structs)
     end
   end
 end
